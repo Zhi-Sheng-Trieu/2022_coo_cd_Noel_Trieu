60438c341874e59e3d61cca458acf586b639a547
Alexandre <alexandre.noel4@etu.univ-lorraine.fr>
2022-05-10 11:15:25 +0200

    fichier txt chargement et test exception

--- test/ChargeurMagasinTest.java
+++ test/ChargeurMagasinTest.java
@@ -1,4 +1,22 @@
+import XML.ChargeurMagasin;
+import donnees.Magasin;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ChargeurMagasinTest {
 
-
+    @Test
+    public void test2_chargerMagasin_exception() {
+        boolean exception = false;
+        try {
+            ChargeurMagasin chargeurMagasin = new ChargeurMagasin("rep_faux");
+            Magasin magasin = chargeurMagasin.chargerMagasin();
+        } catch (FileNotFoundException e){
+            exception = true;
+        }
+        assertEquals(true, exception);
+    }
 }
