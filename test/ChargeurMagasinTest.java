51347a78c0bb269b281bf39f16b4db4e839680cc
Zhi-Sheng <zhishengtrieu@gmail.com>
2022-05-10 11:13:34 +0200

    commit

--- test/ChargeurMagasinTest.java
+++ test/ChargeurMagasinTest.java
@@ -1,4 +1,20 @@
+import XML.ChargeurMagasin;
+import donnees.Magasin;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class ChargeurMagasinTest {
+
+    @Test
+    public void testChargerMagasin() throws FileNotFoundException {
+        ChargeurMagasin chargeur = new ChargeurMagasin("musicbrainzSimple/");
+        Magasin magasin = chargeur.chargerMagasin();
+        assertEquals(12, magasin.getNombreCds());
+    }
 
 
 }
